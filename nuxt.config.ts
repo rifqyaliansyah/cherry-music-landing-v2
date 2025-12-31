d78c0bd49d37ca1829101b3dfa3d6e71973cf9df
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-01-01 00:43:55 +0700

    add pinai dependencies and integrated with project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  modules: ['@pinia/nuxt'],
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL
+    }
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
