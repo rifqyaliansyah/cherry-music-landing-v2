1844479810c21667cb593edcf18fc03060751ac3
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-01-01 20:07:52 +0700

    adjust icon stroke and fill two icon for better UX

--- app/components/MusicPlayerBar.vue
+++ app/components/MusicPlayerBar.vue
@@ -157,7 +157,7 @@ const formatTime = (seconds) => {
                             </button>
 
                             <button @click="$emit('previous')" class="btn btn-square btn-neutral btn-sm md:btn-md">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24"
                                     stroke-width="1.5" stroke="currentColor" class="size-4 shrink-0">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z" />
@@ -165,21 +165,21 @@ const formatTime = (seconds) => {
                             </button>
 
                             <button @click="$emit('toggle-play')" class="btn btn-square btn-neutral btn-lg">
-                                <svg v-if="!isPlaying" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                <svg v-if="!isPlaying" xmlns="http://www.w3.org/2000/svg" fill="white"
                                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                                     class="size-5 md:size-6 shrink-0">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
                                 </svg>
                                 <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="size-5 md:size-6 shrink-0">
+                                    stroke-width="3" stroke="currentColor" class="size-5 md:size-6 shrink-0">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
                                 </svg>
                             </button>
 
                             <button @click="$emit('next')" class="btn btn-square btn-neutral btn-sm md:btn-md">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24"
                                     stroke-width="1.5" stroke="currentColor" class="size-4 shrink-0">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z" />
