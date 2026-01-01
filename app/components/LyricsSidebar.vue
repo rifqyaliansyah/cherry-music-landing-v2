1484a1a1d9950966dacb6f4e592392a2047730c4
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-01-01 14:10:54 +0700

    change icon for handle session instrument

--- app/components/LyricsSidebar.vue
+++ app/components/LyricsSidebar.vue
@@ -222,7 +222,7 @@ const showLyrics = computed(() => {
                         <div v-for="(line, index) in lyricsData" :key="index" :data-index="index"
                             class="lyric-line cursor-pointer" :class="getLineState(index)"
                             @click="handleLyricClick(line.time)">
-                            {{ line.text || '♪' }}
+                            {{ line.text || '♫♫♫' }}
                         </div>
                     </div>
                 </div>
